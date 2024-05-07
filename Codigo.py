2cc0e802dd6b3b6ade951980572f422e6d09e1dd
FelipeMoraes18 <111572342+FelipeMoraes18@users.noreply.github.com>
2024-05-06 21:49:16 -0300

    Login Function

--- Codigo.py
+++ Codigo.py
@@ -13,5 +13,13 @@ def abrir_navegador(url):
     pyautogui.press("enter")
     time.sleep(3) 
 
+def fazer_login(email, senha):
+    pyautogui.click(x=685, y=451) 
+    pyautogui.write(email)
+    pyautogui.press("tab")
+    pyautogui.write(senha)
+    time.sleep(1) 
+    pyautogui.click(x=955, y=660)
+    time.sleep(3)
 
 
